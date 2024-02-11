bc5aa9d2b039f67dc1441da605f7c1c6036739e8
NamigTelliyev <sd7h8wjr6@code.edu.az>
2024-02-12 02:56:14 +0400

    index update

--- assets/JS/home.js
+++ assets/JS/home.js
@@ -20,6 +20,8 @@ function toggleVideo() {
 //     });
 // }
 
+
+//!
 function changeBg(bg, title) {
   const banners = document.querySelectorAll(".banner");
   const contents = document.querySelectorAll(".content");
@@ -41,6 +43,9 @@ function changeBg(bg, title) {
   });
 }
 
+
+
+//!
 $(document).ready(function () {
   $(window).scroll(function () {
     if ($(this).scrollTop() > 900) {
@@ -64,8 +69,83 @@ $(document).ready(function () {
 
 
 
+//!
 $(document).ready(function() {
-  $(".toggleHamburger").click(function() {
+  $(".toggleHamburger").click(function(e) {
+    e.stopPropagation(); // Bu tıklamanın diğer elementlere ulaşmasını engeller
     $(".navbarHamburger").toggleClass("active");
+  });
+
+  // Dokümana herhangi bir yere tıklandığında menünün kapanmasını sağlar
+  $(document).click(function(e) {
+    if (!$(e.target).closest('.hamburgerMenu').length) {
+      $(".navbarHamburger").removeClass("active");
+    }
+  });
+});
+
+
+
+
+//!
+$(document).ready(function() {
+  function handleSearchBarVisibility() {
+    if ($(window).width() >= 992) {
+      $(".searchBar").css("display", "flex"); // Ekran genişliği 992 pikselden büyükse .searchBar görünür olur
+    } else {
+      $(".searchBar").css("display", "none"); // Ekran genişliği 992 pikselden küçükse .searchBar gizlenir
+    }
+  }
+  // Sayfa yüklendiğinde ve ekran boyutu değiştiğinde .searchBar'ın görünürlüğünü kontrol et
+  $(window).on('load resize', handleSearchBarVisibility);
+
+  $(window).scroll(function () {
+    if ($(window).width() < 992) {
+      $(".searchBar").hide(); // Ekran genişliği 992 pikselden küçükse, scroll ile de .searchBar gizlenir
+    }
+  });
+});
+
+
+
+//!
+$(document).ready(function() {
+  $(window).scroll(function() {
+    var footerOffset = $("footer").offset().top;
+    var headerHeight = $(".header").outerHeight();
+
+    if ($(this).scrollTop() > footerOffset - headerHeight) {
+      $(".header").css({
+        opacity: 0,
+        "z-index": -1, // z-index değerini ayarla
+        transition: " 0.4s ease" // Geçiş efekti
+      });
+    } else {
+      $(".header").css({
+        opacity: 1,
+        "z-index": 15, // z-index değerini orijinal değerine geri al
+        transition: " 0.4s ease" // Geçiş efekti
+      });
+    }
+  });
+});
+
+
+//!
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 900) {
+      $(".navSearch").css({
+        opacity: 0,
+        "z-index": -1,
+        transition: "opacity 0.5s ease" // 0.5 saniyelik geçiş efekti
+      });
+    } else {
+      $(".navSearch").css({
+        opacity: 1,
+        "z-index": 15,
+        transition: "opacity 0.5s ease" // 0.5 saniyelik geçiş efekti
+      });
+    }
   });
 });
